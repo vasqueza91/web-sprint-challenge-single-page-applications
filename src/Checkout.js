a475f1ed542c520d928c53eea2bb6b43e9a21a2f
Alejandro Vasquez <alejandrovasquez@Alejandros-iMac.local>
2021-06-18 05:01:56 -0700

    Fixing

--- src/Checkout.js
+++ src/Checkout.js
@@ -9,11 +9,11 @@ function Order({ details }) {
     <div className='order container'>
       <h2>{details.name}</h2>
       <p>Size: {details.size}</p>
-      <p>Sauce: {details.sauce}</p>
-      <p>Pepperoni: {details.pepperoni}</p>
-      <p>Ham: {details.ham}</p>
-      <p>Olives: {details.olives}</p>
-      <p>Bacon: {details.bacon}</p>
+      {/* <p>Sauce: {details.sauce}</p> */}
+      <p>Pepperoni {details.pepperoni}</p>
+      <p>Ham {details.ham}</p>
+      <p>Olives {details.olives}</p>
+      <p>Bacon {details.bacon}</p>
       
 
       {/* {
